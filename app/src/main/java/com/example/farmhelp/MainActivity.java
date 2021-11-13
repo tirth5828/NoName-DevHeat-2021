e6be07920765d37f8dc3b8b8a3fe94766552a0c9
foram-patel-0201 <foramjpatel0201@gmail.com>
2021-11-13 11:51:16 +0530

    Current Weather Data Added

--- app/src/main/java/com/example/farmhelp/MainActivity.java
+++ app/src/main/java/com/example/farmhelp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.farmhelp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         
     }
 
     public void MoveOn(View view) {
         Intent intent = new Intent(this, MainLanding.class);
         startActivity(intent);
+    }
+
+    public void getWeather(View view){
+        startActivity(new Intent(this,Weather.class));
     }
 }
---
+++ app/src/main/java/com/example/farmhelp/Weather.java
@@ -0,0 +1,211 @@
+package com.example.farmhelp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.Manifest;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Weather extends AppCompatActivity  {
+
+    TextView windspeed,tempMin,tempMax,pressure,humidity;
+    double Lat,Lon;
+    FusedLocationProviderClient mFusedLocationClient;
+    int REQUEST_LOCATION = 1;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        windspeed = findViewById(R.id.windspeed);
+        tempMin = findViewById(R.id.tempMin);
+        tempMax = findViewById(R.id.tempMax);
+        pressure = findViewById(R.id.pressure);
+        humidity = findViewById(R.id.humidity);
+
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+
+    }
+
+    public void get(View v){
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat="+Lat+"&lon="+Lon+"&appid=4ef8329ad2c5e87ab137d6be93db1f42";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
+
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    JSONObject array= (JSONObject) response.get("wind");
+                    JSONObject array1= (JSONObject) response.get("main");
+
+
+
+
+                    String s1= array.getString("speed");
+                    String s2= array1.getString("temp_min");
+                    String s3= array1.getString("temp_max");
+                    String s4= array1.getString("pressure");
+                    String s5= array1.getString("humidity");
+
+
+
+                    windspeed.setText(s1);
+                    tempMin.setText(s2);
+                    tempMax.setText(s3);
+                    pressure.setText(s4);
+                    humidity.setText(s5);
+
+                    //Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    windspeed.setText(e.getMessage());
+                    tempMin.setText(e.getMessage());
+                    tempMax.setText(e.getMessage());
+                    pressure.setText(e.getMessage());
+                    humidity.setText(e.getMessage());
+
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                windspeed.setText(error.toString());
+                tempMin.setText(error.toString());
+                tempMax.setText(error.toString());
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(request);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            Lat = location.getLatitude();
+                            Lon = location.getLongitude();
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            Lat = mLastLocation.getLatitude();
+            Lon = mLastLocation.getLongitude();
+        }
+    };
+
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
+    }
